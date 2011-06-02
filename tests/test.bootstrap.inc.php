c6bbb2d9b52b04a67f9a6f4ba8c39354e73a65c8
Heinz Wiesinger <heinz@m2mobi.com>
2011-06-02 16:29:14 +0200

    tests: Fix unittests after namespace introduction

--- tests/test.bootstrap.inc.php
+++ tests/test.bootstrap.inc.php
@@ -5,14 +5,13 @@ $base = dirname(__FILE__) . "/..";
 set_include_path(
     $base . "/config:" .
     $base . "/system/config:" .
-    $base . "/system/libraries/core:" .
-    $base . "/system/libraries/third-party:" .
+    $base . "/system:" .
     get_include_path()
 );
 
 // Load and setup class file autloader
-include_once("class.autoloader.inc.php");
-spl_autoload_register("Autoloader::load");
+include_once("libraries/core/class.autoloader.inc.php");
+spl_autoload_register("Lunr\Libraries\Core\Autoloader::load");
 
 
 ?>
