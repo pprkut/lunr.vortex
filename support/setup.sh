ed17a5b5d2e468637e8100ed337da53bfb1c1f05
Heinz Wiesinger <heinz@m2mobi.com>
2016-10-14 17:06:07 +0200

    General: Add Requests as a new dependency to replace the Curl class

--- support/setup.sh
+++ support/setup.sh
@@ -13,6 +13,7 @@ fi
 PSRLOG="https://github.com/php-fig/log/archive/1.0.0.tar.gz"
 APNSPHP="https://github.com/M2Mobi/ApnsPHP/archive/b9c04fdfdf63da714fbb06e4151d939e0e9fdccb.tar.gz"
 PHPMAILER="https://github.com/PHPMailer/PHPMailer/archive/v6.0.0rc2.tar.gz"
+REQUESTS="https://github.com/rmccue/Requests/archive/v1.7.0.tar.gz"
 
 if ! [ -e "$DESTDIR/Psr/Log" ]; then
   cd "$TMP"
@@ -44,6 +45,15 @@ if ! [ -e "$DESTDIR/PHPMailer" ]; then
 
     mkdir -p "$DESTDIR/PHPMailer"
     mv PHPMailer-6.0.0rc2/src "$DESTDIR/PHPMailer/PHPMailer"
+  cd -
+fi
+
+if ! [ -e "$DESTDIR/Requests" ]; then
+  cd "$TMP"
+    wget --content-disposition "$REQUESTS"
+    tar xvf Requests-1.7.0.tar.gz
+
+    mv Requests-1.7.0/library "$DESTDIR/Requests"
   cd -
 fi
 
