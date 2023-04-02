9a4dff96dd479178d1e083612301b90983d83ee7
Heinz Wiesinger <heinz@m2mobi.com>
2023-04-02 15:02:03 +0200

    General: Add copyright and license info to comply with REUSE v3.0
    Reviewed at https://reviews.lunr.nl/r/944/

--- tests/phan.config.php
+++ tests/phan.config.php
@@ -1,6 +1,8 @@
 <?php
 
 /**
+ * Phan configuration file.
+ *
  * SPDX-FileCopyrightText: Copyright 2020 M2mobi B.V., Amsterdam, The Netherlands
  * SPDX-FileCopyrightText: Copyright 2022 Move Agency Group B.V., Zwolle, The Netherlands
  * SPDX-License-Identifier: MIT
